42294c8decbb63176053e0c8b6b3e83f4a493dd3
anurag <anuragmait20004@gmail.com>
2016-12-14 09:13:37 -0500

    Just working on modules

--- OdeToFood2/Controllers/ReviewsController.cs
+++ OdeToFood2/Controllers/ReviewsController.cs
@@ -1,128 +1,51 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
-//using OdeToFood2.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood2.Models;
 
-//namespace OdeToFood2.Controllers
-//{
-//    public class ReviewsController : Controller
-//    {
+namespace OdeToFood2.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        OdeToFoodDb _db = new OdeToFoodDb();
+        // GET: Reviews
+        public ActionResult Index([Bind(Prefix ="id")]int restaurantId = 0)
+        {
+            var restaurant = _db.Restaurants.Find(restaurantId);
+            if (restaurant == null)
+            {
+                return new HttpNotFoundResult();
+            } 
+            return View(restaurant);
+        }
 
-//        public ActionResult BestReview()
-//        {
-//            var bestReview = from r in _reviews
-//                orderby r.Rating descending
-//                select r;
-//            return PartialView("_Review", bestReview.First());
-//        }
+        [HttpGet]
+        public ActionResult Create(int restaurantId = 0)
+        {
+            return View();
+        }
 
-//        // GET: Reviews
-//        public ActionResult Index()
-//        {
-//            var model = from r in _reviews
-//                orderby r.Country
-//                select r;
-//            return View(model);
-//        }
+        [HttpPost]
+        public ActionResult Create(RestaurantReview review)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Reviews.Add(review);
+                _db.SaveChanges();
+                return RedirectToAction("Index", new { id = review.RestaurantId });
+            }
+            return View(review);
+        }
 
-//        // GET: Reviews/Details/5
-//        public ActionResult Details(int id)
-//        {
-//            return View();
-//        }
-
-//        // GET: Reviews/Create
-//        public ActionResult Create()
-//        {
-//            return View();
-//        }
-
-//        // POST: Reviews/Create
-//        [HttpPost]
-//        public ActionResult Create(FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add insert logic here
-
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-
-//        // GET: Reviews/Edit/5
-//        public ActionResult Edit(int id)
-//        {
-//            var review = _reviews.Single(r => r.Id == id);
-//            return View(review);
-//        }
-
-//        // POST: Reviews/Edit/5
-//        [HttpPost]
-//        public ActionResult Edit(int id, FormCollection collection)
-//        {
-//            var review = _reviews.Single(r => r.Id == id);
-//            if (TryUpdateModel(review))
-//            {
-//                return RedirectToAction("Index"); 
-//            }
-//            return View(review);
-//        }
-
-//        // GET: Reviews/Delete/5
-//        public ActionResult Delete(int id)
-//        {
-//            return View();
-//        }
-
-//        // POST: Reviews/Delete/5
-//        [HttpPost]
-//        public ActionResult Delete(int id, FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add delete logic here
-
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-
-//        static List<RestaurantReview> _reviews = new List<RestaurantReview>
-//        {
-//            new RestaurantReview
-//            {
-//                Id = 1,
-//                Name = "Cinnamon club",
-//                City = "Longdon",
-//                Country = "UK",
-//                Rating = 10,
-//            },
-//            new RestaurantReview
-//            {
-//                Id = 2,
-//                Name = "Two ub",
-//                City = "columbumdon",
-//                Country = "USA",
-//                Rating = 8,
-//            },
-//            new RestaurantReview
-//            {
-//                Id = 3,
-//                Name = "club",
-//                City = "Longdon",
-//                Country = "Germany",
-//                Rating = 4,
-//            },
-
-//        };
-//    }
-//}
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
